42fcfcb98bb4ef3dcacb08021b5a6353ee086087
Shashank Agarwal <shashank1010@gmail.com>
2023-02-02 15:45:07 +0300

    Add screens

--- src/Navigation/NavigationContainer.tsx
+++ src/Navigation/NavigationContainer.tsx
@@ -1,27 +1,37 @@
-import { useMolecules } from '@bambooapp/bamboo-molecules';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { FC, useMemo } from 'react';
+import type { FC } from 'react';
+import { useMemo } from 'react';
 
 import { Example } from '~/Example';
+import type { TodoListState } from '~/store/state.types';
 
-const HomeScreen: FC = _props => {
-    const { View } = useMolecules();
-    const style = useMemo(() => ({ padding: 'spacings.8' }), []);
-    return (
-        <View style={style}>
-            <Example />
-        </View>
-    );
-};
+const getScreen =
+    (props): FC =>
+    _props => {
+        return <Example {...props} />;
+    };
 
 const Drawer = createDrawerNavigator();
 
 export default function Navigator() {
+    const { listScreen, completedScreen } = useMemo(
+        () => ({
+            listScreen: getScreen({
+                filter: (records: TodoListState['todos']) => records,
+            }),
+            completedScreen: getScreen({
+                filter: (records: TodoListState['todos']) => records.filter(({ isDone }) => isDone),
+            }),
+        }),
+        [],
+    );
+
     return (
         <NavigationContainer>
             <Drawer.Navigator>
-                <Drawer.Screen name="Home" component={HomeScreen} />
+                <Drawer.Screen name="all" children={listScreen} />
+                <Drawer.Screen name="completed" children={completedScreen} />
             </Drawer.Navigator>
         </NavigationContainer>
     );
